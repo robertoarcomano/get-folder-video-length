4f78e037ad849d690299e6494e6ad120c9af6b76
Roberto Arcomano <info@robertoarcomano.com>
2024-03-08 16:22:49 +1300

    1. Created class 2. Simplified

--- get_video_length.py
+++ get_video_length.py
@@ -4,44 +4,57 @@ import datetime
 import math
 import sys
 
-length = 0
-current_file = 0
 
+class GetVideoLength:
+    file_formats = [".mp4"]
 
-def get_num_files(directory):
-    n = 0
-    for root, dirs, files in os.walk(directory):
-        for name in files:
-            filename = os.path.join(root, name)
-            if ".mp4" in filename:
-                n += 1
-    return n
+    def __init__(self, video_directory):
+        self.video_directory, self.num_files = video_directory, self.get_num_files(video_directory)
 
+    def get_num_files(self, directory):
+        n = 0
+        for root, dirs, files in os.walk(directory):
+            for name in files:
+                filename = os.path.join(root, name)
+                for file_format in self.file_formats:
+                    if file_format in filename:
+                        n += 1
+                        break
+        return n
 
-def get_durations_lazy(directory):
-    for root, dirs, files in os.walk(directory):
-        for name in files:
-            filename = os.path.join(root, name)
-            if ".mp4" in filename:
-                yield float(ffmpeg.probe(filename)["streams"][0]["duration"])
+    def no_files(self):
+        return self.num_files == 0
 
+    def get_durations(self):
+        for root, dirs, files in os.walk(self.video_directory):
+            for name in files:
+                filename = os.path.join(root, name)
+                if ".mp4" in filename:
+                    yield float(ffmpeg.probe(filename)["streams"][0]["duration"])
 
-def perc(curr, num):
-    return str(math.ceil(curr * 100 / num)) + "%"
+    @staticmethod
+    def perc(curr, num):
+        return str(math.ceil(curr * 100 / num)) + "%"
+
+    def show_duration(self):
+        length = 0
+        current_file = 0
+        for duration in self.get_durations():
+            print("\rDuration:", str(datetime.timedelta(seconds=length)).split(".")[0], self.perc(current_file, self.num_files),
+                  end="")
+            length += duration
+            current_file += 1
 
 
 if len(sys.argv) < 2:
     print("Syntax:", sys.argv[0], "<Video Directory>")
     exit(-1)
 
-VIDEO_DIR = sys.argv[1]
-num_files = get_num_files(VIDEO_DIR)
-if num_files == 0:
-    print("No video files in", VIDEO_DIR)
+get_video_length = GetVideoLength(sys.argv[1])
+if get_video_length.no_files():
+    print("No video files in", sys.argv[1])
     exit(-2)
-print("num_files:", num_files)
-for duration in get_durations_lazy(VIDEO_DIR):
-    print("\rDuration:", str(datetime.timedelta(seconds=length)).split(".")[0], perc(current_file, num_files), end="")
-    length += duration
-    current_file += 1
+print("num_files:", get_video_length.num_files)
+get_video_length.show_duration()
+
 
