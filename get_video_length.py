54abfd1ec9c396659261be221479c086b5b59312
Roberto Arcomano <info@robertoarcomano.com>
2024-03-11 16:19:05 +1300

    Final version

--- get_video_length.py
+++ get_video_length.py
@@ -11,20 +11,16 @@ class GetVideoLength:
 
     def __init__(self, video_directory):
         self.video_directory = video_directory
-        self.set_filenames()
+        self.get_filenames()
 
-    def set_filenames(self):
+    def get_filenames(self):
         for root, dirs, files in os.walk(self.video_directory):
             for name in files:
-                filename = os.path.join(root, name)
-                if any(file_format in filename for file_format in self.file_formats):
-                    self.filenames.append(filename)
+                if any(file_format in name for file_format in self.file_formats):
+                    self.filenames.append(os.path.join(root, name))
 
     def num_files(self):
         return len(self.filenames)
-
-    def no_files(self):
-        return not self.num_files()
 
     def get_durations(self):
         for filename in self.filenames:
@@ -35,15 +31,20 @@ class GetVideoLength:
         return str(math.ceil(curr * 100 / num)) + "%"
 
     def sum_durations(self):
-        output = ""
         length = 0
         current_file = 0
         yield "num_files: " + str(self.num_files()) + "\n"
         for duration in self.get_durations():
-            yield "\rDuration: " + str(datetime.timedelta(seconds=length)).split(".")[0] + " " + \
-                self.perc(current_file, self.num_files())
             length += duration
             current_file += 1
+            yield "\rDuration: " + str(datetime.timedelta(seconds=length)).split(".")[0] + " " + \
+                self.perc(current_file, self.num_files())
+
+    def get_sum_durations_in_seconds(self):
+        length = 0
+        for duration in self.get_durations():
+            length += duration
+        return length
 
 
 if __name__ == "__main__":
@@ -51,9 +52,10 @@ if __name__ == "__main__":
         print("Syntax:", sys.argv[0], "<Video Directory>")
         exit(-1)
     get_video_length = GetVideoLength(sys.argv[1])
-    if get_video_length.no_files():
+    if not get_video_length.num_files():
         print("No video files in", sys.argv[1])
         exit(-2)
     for row in get_video_length.sum_durations():
         print(row, end="")
     print()
+    print("Seconds:", get_video_length.get_sum_durations_in_seconds())
--- test_get_video_length.py
+++ test_get_video_length.py
@@ -3,11 +3,13 @@ from get_video_length import GetVideoLength
 
 def test_get_video_length():
     get_video_length = GetVideoLength("video_test")
-    assert get_video_length.no_files() is False
+    assert get_video_length.num_files() > 0
     expected = [
         "num_files: 2\n",
-        "\rDuration: 0:00:00 0%",
-        "\rDuration: 0:00:09 50%"
+        "\rDuration: 0:00:09 50%",
+        "\rDuration: 0:00:30 100%"
     ]
+    seconds = 30.447533
     rows = list(get_video_length.sum_durations())
     assert rows == expected
+    assert get_video_length.get_sum_durations_in_seconds() == seconds
